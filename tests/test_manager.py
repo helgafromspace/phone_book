69e44a63339d2abcb18103a285e71089c1e2954a
olhasemenova <olha.sergeevna87@gmail.com>
2023-08-15 18:00:13 -0400

    add CreateNewUser TestCase

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -6,7 +6,7 @@ from manager import create, update, delete,restore_deleted_values
 from search import search_record
 from main import main
 from unittest.mock import patch
-class CreateNewUser(TestCase):
+class CreateUpdateDeleteUser(TestCase):
 
     def setUp(self) -> None:
         self.filepath = 'database_test.json'
@@ -14,21 +14,52 @@ class CreateNewUser(TestCase):
         self.dataset = read_dataset(self.filepath)
         self.user_data = ['John', 'Smith', 'LA', 'USA']
 
-    def test_create_new_user(self,):
-            input_list = [self.phone, 'John', 'Smith', 'LA', 'USA']
-            inputs = iter(input_list)
-            with patch("builtins.input", side_effect=inputs):
-                data = read_values()
-                self.dataset = create(self.dataset, data)
-            print(self.dataset)
+    def test_create_new_user(self):
+        input_list = [self.phone, 'John', 'Smith', 'LA', 'USA']
+        inputs = iter(input_list)
+        with patch("builtins.input", side_effect=inputs):
+            data = read_values()
+            self.dataset = create(self.dataset, data)
+        userdata_in_dataset = ([self.dataset[self.phone]['first_name'], self.dataset[self.phone]['last_name'],
+                                self.dataset[self.phone]['city'], self.dataset[self.phone]['country']])
+        self.assertEqual(*self.dataset.keys(), self.phone)
+        self.assertSequenceEqual(self.user_data, userdata_in_dataset)
 
     def test_update_user_first_name(self):
-        pass
+        new_fname = 'Jack'
+        input_list = [self.phone, new_fname, '', '', '']
+        inputs = iter(input_list)
+        with patch("builtins.input", side_effect=inputs):
+            update(self.dataset)
+        self.assertEqual(self.dataset[self.phone]['first_name'], new_fname)
 
+    def test_update_user_last_name(self):
+        new_lname = 'King'
+        input_list = [self.phone, '', new_lname, '', '']
+        inputs = iter(input_list)
+        with patch("builtins.input", side_effect=inputs):
+            update(self.dataset)
+        self.assertEqual(self.dataset[self.phone]['last_name'], new_lname)
 
+    def test_update_user_city(self):
+        new_city = 'Monreal'
+        input_list = [self.phone, '', '', new_city, '']
+        inputs = iter(input_list)
+        with patch("builtins.input", side_effect=inputs):
+            update(self.dataset)
+        self.assertEqual(self.dataset[self.phone]['city'], new_city)
 
-    # def tearDown(self) -> None:
-    #     write_dataset(self.dataset, self.filepath)
+    def test_update_user_country(self):
+        new_country = 'Canada'
+        input_list = [self.phone, '', '', '', new_country]
+        inputs = iter(input_list)
+        with patch("builtins.input", side_effect=inputs):
+            update(self.dataset)
+        self.assertEqual(self.dataset[self.phone]['country'], new_country)
+
+    def tearDown(self) -> None:
+        write_dataset(self.dataset, self.filepath)
+
 if __name__ == "__main__":
     unittest.main()
 
