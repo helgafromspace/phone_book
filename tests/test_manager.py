b5dd543bbc6b49c6d7d5c1fe706c353983cfed61
olhasemenova <olha.sergeevna87@gmail.com>
2023-08-15 18:49:21 -0400

    add CreateUpdateDeleteNewUser TestCase

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -10,52 +10,55 @@ class CreateUpdateDeleteUser(TestCase):
 
     def setUp(self) -> None:
         self.filepath = 'database_test.json'
-        self.phone = '380998526455'
+        self.phone1 = '380998526455'
+        self.phone2 = '380637786375'
         self.dataset = read_dataset(self.filepath)
         self.user_data = ['John', 'Smith', 'LA', 'USA']
+        self.user_data2 = ['Lucy', 'Jackson', 'LA', 'USA']
 
     def test_create_new_user(self):
-        input_list = [self.phone, 'John', 'Smith', 'LA', 'USA']
+        input_list = [self.phone1, 'John', 'Smith', 'LA', 'USA']
         inputs = iter(input_list)
         with patch("builtins.input", side_effect=inputs):
             data = read_values()
             self.dataset = create(self.dataset, data)
-        userdata_in_dataset = ([self.dataset[self.phone]['first_name'], self.dataset[self.phone]['last_name'],
-                                self.dataset[self.phone]['city'], self.dataset[self.phone]['country']])
-        self.assertEqual(*self.dataset.keys(), self.phone)
+        userdata_in_dataset = ([self.dataset[self.phone1]['first_name'], self.dataset[self.phone1]['last_name'],
+                                self.dataset[self.phone1]['city'], self.dataset[self.phone1]['country']])
+        self.assertEqual(*self.dataset.keys(), self.phone1)
         self.assertSequenceEqual(self.user_data, userdata_in_dataset)
 
     def test_update_user_first_name(self):
         new_fname = 'Jack'
-        input_list = [self.phone, new_fname, '', '', '']
+        input_list = [self.phone1, new_fname, '', '', '']
         inputs = iter(input_list)
         with patch("builtins.input", side_effect=inputs):
             update(self.dataset)
-        self.assertEqual(self.dataset[self.phone]['first_name'], new_fname)
+        self.assertEqual(self.dataset[self.phone1]['first_name'], new_fname)
 
     def test_update_user_last_name(self):
         new_lname = 'King'
-        input_list = [self.phone, '', new_lname, '', '']
+        input_list = [self.phone1, '', new_lname, '', '']
         inputs = iter(input_list)
         with patch("builtins.input", side_effect=inputs):
             update(self.dataset)
-        self.assertEqual(self.dataset[self.phone]['last_name'], new_lname)
+        self.assertEqual(self.dataset[self.phone1]['last_name'], new_lname)
 
     def test_update_user_city(self):
         new_city = 'Monreal'
-        input_list = [self.phone, '', '', new_city, '']
+        input_list = [self.phone1, '', '', new_city, '']
         inputs = iter(input_list)
         with patch("builtins.input", side_effect=inputs):
             update(self.dataset)
-        self.assertEqual(self.dataset[self.phone]['city'], new_city)
+        self.assertEqual(self.dataset[self.phone1]['city'], new_city)
+
 
     def test_update_user_country(self):
         new_country = 'Canada'
-        input_list = [self.phone, '', '', '', new_country]
+        input_list = [self.phone1, '', '', '', new_country]
         inputs = iter(input_list)
         with patch("builtins.input", side_effect=inputs):
             update(self.dataset)
-        self.assertEqual(self.dataset[self.phone]['country'], new_country)
+        self.assertEqual(self.dataset[self.phone1]['country'], new_country)
 
 
     def test_fail_update_with_incorrect_number(self):
@@ -67,8 +70,23 @@ class CreateUpdateDeleteUser(TestCase):
             err = update(self.dataset)
         self.assertEqual(err, 'Номер відстуній в книжці')
 
+    def create_second_user(self):
+        input_list = [self.phone2, 'Lucy', 'Jackson', 'LA', 'USA']
+        inputs = iter(input_list)
+        with patch("builtins.input", side_effect=inputs):
+            data = read_values()
+            create(self.dataset, data)
+        userdata_in_dataset = ([self.dataset[self.phone2]['first_name'], self.dataset[self.phone2]['last_name'],
+                                self.dataset[self.phone2]['city'], self.dataset[self.phone2]['country']])
+    def test_delete_user(self):
+        self.create_second_user()
+        with patch("builtins.input", return_value=self.phone2):
+            delete(self.dataset)
+
+
     def tearDown(self) -> None:
         write_dataset(self.dataset, self.filepath)
+
 
 if __name__ == "__main__":
     unittest.main()
